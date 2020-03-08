67be21db78f7fa22dd209e38b939dca0ce59f219
Kleutsevich <test@email.com>
2020-03-09 00:33:22 +0300

    I don't have enough time (

--- src/jc01_2020/Kleautsevich/lesson10/task01/Application.java
+++ src/jc01_2020/Kleautsevich/lesson10/task01/Application.java
@@ -7,10 +7,19 @@ package jc01_2020.Kleautsevich.lesson10.task01;
  *
  */
 
+import java.util.HashSet;
+
 public class Application {
 
 	public static void main(String[] args) {
-
+		HashSet<String> wordsHashSet = new HashSet<>();
+		wordsHashSet.add("one");
+		wordsHashSet.add("two");
+		wordsHashSet.add("three");
+		wordsHashSet.add("four");
+		wordsHashSet.add("five");
+		System.out.println(wordsHashSet.size());
+		wordsHashSet.add("two");
+		System.out.println(wordsHashSet.size());
 	}
-
 }
